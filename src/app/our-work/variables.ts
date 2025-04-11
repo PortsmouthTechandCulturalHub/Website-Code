272eeec010ec0566953f08ea19f12f96be3754dd
Adioo Technology <151016818+adiootechnology@users.noreply.github.com>
2025-04-11 03:04:40 -0700

    text update

--- src/app/our-work/variables.ts
+++ src/app/our-work/variables.ts
@@ -37,18 +37,18 @@ export type Work = {
 export const work: Work[] = [
   {
     id: 0,
-    title: "Lorem Ipsum",
-    desc: "Lorem Ipsum has been the industry' standard dummy ",
+    title: "Digital Skills & Workforce Empowerment",
+    desc: "Building Tech Confidence, One Workshop at a Time We believe everyone deserves access to the tools and knowledge that shape the future. That’s why we offer inclusive training in essential digital skills—from coding and cybersecurity to AI literacy and data storytelling. Whether you're a young person exploring tech for the first time, a jobseeker looking to upskill, or a community group wanting to digitize your impact, we provide practical, hands-on learning opportunities that open doors. Our workshops, bootcamps, and mentoring programs are designed to boost confidence, unlock potential, and support meaningful career progression—locally and globally.",
   },
   {
     id: 1,
-    title: "Lorem Ipsum",
-    desc: "Lorem Ipsum has been the industry' standard dummy ",
+    title: "Creative Technology & Cultural Innovation",
+    desc: "Merging Creativity with Code to Tell New Stories Portsmouth is rich in culture—and we’re bringing it into the digital age. Through creative tech, we empower artists, storytellers, and cultural organizations to experiment with digital tools like AR, VR, interactive media, and AI-generated art. We run labs and events that blend tradition with innovation, allowing communities to explore new forms of expression. From immersive heritage experiences to indie game development and music-tech mashups, we celebrate the diverse voices shaping our city’s cultural and creative future.",
   },
   {
     id: 2,
-    title: "Lorem Ipsum",
-    desc: "Lorem Ipsum has been the industry' standard dummy ",
+    title: "Tech for Good & Community Innovation",
+    desc: "Technology that Works for People and the Plane At the heart of everything we do is a commitment to social impact. We use technology as a force for good—solving local challenges, empowering underrepresented voices, and promoting equity in the digital world. Through hackathons, civic tech projects, and collaborative problem-solving, we work alongside residents, local authorities, educators, and nonprofits to co-create digital solutions that make everyday life better. Whether it’s building a community platform, supporting grassroots entrepreneurs, or helping older adults navigate the digital world, we’re here to make tech more human—and more helpful. ",
   },
   {
     id: 3,
