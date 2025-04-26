e7638f7d09c8effb8b2f1a8eead4c29432f22d00
Adioo Technology <151016818+adiootechnology@users.noreply.github.com>
2025-04-26 06:34:58 -0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { hero, values, stat, blog, vision } from "@/app/variables";
 import BlogPreview from "@/components/content/home/blog-preview";
 import Hero from "@/components/content/home/hero";
 import Stats from "@/components/content/home/stats";
-import Vision from "@/components/content/home/vision";
 import Values from "@/components/content/home/values";
+import Vision from "@/components/content/home/vision";
 
 export default function Home() {
   return (
