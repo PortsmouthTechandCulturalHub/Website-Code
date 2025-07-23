8ef3ee726ecc38c7e76b36ab7fe8abc698616011
Adioo Technology <151016818+adiootechnology@users.noreply.github.com>
2025-07-23 11:31:33 +0100

    wait

--- .next/types/app/blog/[id]/page.ts
+++ .next/types/app/blog/[id]/page.ts
@@ -1,10 +1,11 @@
 // File: /Users/user/Desktop/portsmouth/src/app/blog/[id]/page.tsx
-import * as entry from '../../../../../src/app/blog/[id]/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
+
+import * as entry from '../../../../../src/app/blog/[id]/page.js'
 
 type TEntry = typeof import('../../../../../src/app/blog/[id]/page.js')
 
-type SegmentParams<T extends Object = any> = T extends Record<string, any>
+type SegmentParams<T extends object = any> = T extends Record<string, any>
   ? { [K in keyof T]: T[K] extends string ? string | string[] | undefined : never }
   : T
 
